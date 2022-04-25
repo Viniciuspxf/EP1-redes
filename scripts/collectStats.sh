b429f0a4a8b4a27ff7d8416a6e1605af41a71fbf
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-04-25 18:33:07 -0300

    finish stats collection

--- scripts/collectStats.sh
+++ scripts/collectStats.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+rm results/$1/output.txt
 while [ 1 -eq 1 ]; do
-  docker stats --no-stream --format "{{.CPUPerc}} | {{.NetIO}}" ep1_ep1_1 >> results/$1/output.txt
+  docker stats --no-stream --format "{{.CPUPerc}} | {{.NetIO}}"  >> results/$1/output.txt
 done
--- scripts/doMeasurements.sh
+++ scripts/doMeasurements.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
+make filter
 
 CASES=(0 100 1000)
 cd ..
+
 rm -r results
 
 mkdir results
 
 for NUMBER_OF_CLIENTS in ${CASES[@]}; do
   mkdir results/${NUMBER_OF_CLIENTS}
-  for _ in $(seq 1); do
+  for _ in $(seq 15); do
     docker-compose up -d
 
+    ./scripts/collectStats.sh ${NUMBER_OF_CLIENTS} &
     for COUNTER in $(seq $(expr $NUMBER_OF_CLIENTS / 2)); do
-      mosquitto_sub -t "luminosity" -t "temperature" &
-      mosquitto_sub -t "sound" &
+      mosquitto_sub -p 8000 -t "luminosity" -t "temperature" &
+      mosquitto_sub -p 8000 -t "sound" &
     done
 
-    ./scripts/collectStats.sh ${NUMBER_OF_CLIENTS} &
     for COUNTER in $(seq $(expr $NUMBER_OF_CLIENTS / 3)); do
-      mosquitto_pub -t "luminosity" -m "${COUNTER}Lumen"
-      mosquitto_pub -t "temperature" -m "${COUNTER}Celsius"
-      mosquitto_pub -t "sound" -m "${COUNTER}dB"
+      mosquitto_pub -p 8000 -t "luminosity" -m "${COUNTER}Lumen"
+      mosquitto_pub -p 8000 -t "temperature" -m "${COUNTER}Celsius"
+      mosquitto_pub -p 8000 -t "sound" -m "${COUNTER}dB"
     done
-    kill -9 $(pgrep collectStats) collectStats 2> /dev/null
+    sleep 3
+    kill -9 $(pgrep collect) 2> /dev/null
+    sleep 3
+    ./scripts/filter < results/${NUMBER_OF_CLIENTS}/output.txt >> results/${NUMBER_OF_CLIENTS}/outputFiltered.txt
     kill -9 $(pgrep mosquitto_sub) 2> /dev/null
     docker-compose down
   done
